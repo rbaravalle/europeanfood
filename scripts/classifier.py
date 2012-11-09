0eb5e5cbd6d911f2f82e4e0a4e67c48b6fb7639a
rbaravalle <rbaravalle@gmail.com>
2012-11-09 18:42:59 -0300

    random forest

--- scripts/classifier.py
+++ scripts/classifier.py
@@ -8,8 +8,15 @@ import colortransforms
 import numpy as np
 from featureTexture import *
 
-def features(filename,i):
+def callF(filename,which):
+    return features(filename,which,3,False)
+
+def features(filename,i,j,combine):
     farr = [colorHistogram,ccv,haralick,lbp,tas,zernike]
+
+    if(combine==True):
+        return hstack((farr[1](filename),farr[2](filename),farr[3](filename),farr[4](filename),farr[5](filename)))
+
     return farr[i](filename)
 
 
@@ -46,7 +53,8 @@ def getCross(fileStxt):
         if not line: break
     return cross
 
-def test(fileStxt, fileCtxt, base):
+
+def csvm(dtrain,dtest,labels,fileStxt, fileCtxt, base):
     arch = base+'.predict'
 
     easy = './easy.py'
@@ -81,12 +89,30 @@ def test(fileStxt, fileCtxt, base):
         c = c+1
         if not line: break
 
-    a = [i for i in range(len(testL)-1)]
-    a = map(lambda i: i/20+1, a)
+    return testL
+
+def crandomforest(dtrain,dtest,labels,fileStxt, fileCtxt, base):
+    from sklearn.ensemble import RandomForestClassifier
+    clf = RandomForestClassifier(n_estimators=60)
+    clf.fit(dtrain,labels)
+    print "Random Forest: ", clf.score(dtest,labels)
+    return np.array(clf.predict(dtest)).astype(np.int32)
+
+
+def classifierPredict(dtrain,dtest,labels,fileStxt, fileCtxt, base, i):
+    pred = [csvm, crandomforest]
+    return pred[i](dtrain,dtest,labels,fileStxt, fileCtxt, base)
+
+
+def test(dtrain,dtest,labels,fileStxt, fileCtxt, base):
+    # 0: svm
+    # 1: random forest
+
+    testL = classifierPredict(dtrain,dtest,labels,fileStxt, fileCtxt, base,1)
 
     print "Test: ", testL
 
-    b = conf_mat(testL,a)
+    b = conf_mat(testL,labels)
     for row in b:
         print row
 
@@ -94,7 +120,7 @@ def test(fileStxt, fileCtxt, base):
 
 
 
-def main(subname,which):
+def main(subname,which,local):
     base = subname+'C.txt'
     fileStxt = '../exps/'+subname+'S.txt'
     fileScsv = '../exps/'+subname+'S.csv'
@@ -114,88 +140,94 @@ def main(subname,which):
 
     path = '../images/nonbread/res/'
     dirList=os.listdir(path)
-    print len(dirList)
+    #print len(dirList)
     
     nonbread = [['Df' for j in range(dDFs)] for i in range(len(dirList))]
 
 
-    local = True
     if(local == True):
-        trainingWords, testingWords = localFeatures(subname)
+        trainingData, testingData = localFeatures(subname)
         with open(fileScsv, 'wb') as f:
             writer = csv.writer(f)
-            writer.writerows(trainingWords)
+            writer.writerows(trainingData)
 
         with open(fileCcsv, 'wb') as f:
             writer = csv.writer(f)
-            writer.writerows(testingWords)
+            writer.writerows(testingData)
 
         prog = './a.out' # convert.c
         cmd = '{0} "{1}" > "{2}"'.format(prog, fileScsv, fileStxt)
         Popen(cmd, shell = True, stdout = PIPE).communicate()	
         cmd = '{0} "{1}" > "{2}"'.format(prog, fileCcsv, fileCtxt)
         Popen(cmd, shell = True, stdout = PIPE).communicate()
-        test(fileStxt, fileCtxt, base)
+        labels = [i for i in range(len(testingData))]
+        labels = map(lambda i: i/20+1, labels)
+        test(trainingData, testingData, labels, fileStxt, fileCtxt, base)
         return
 
-    # else global
-    import Image
-    j = 0
-    for i in range(len(dirList)):
-        filename = path+dirList[i]
-        I = Image.open(filename)
-        if(I.mode == 'RGB'):
+    else:    # else global
+        import Image
+        j = 0
+        for i in range(len(dirList)):
+            filename = path+dirList[i]
+            I = Image.open(filename)
+            if(I.mode == 'RGB'):
+                print filename
+                nonbread[j] = callF(filename,which)
+                j = j+1
+            if (j > cant*2+1):
+                break
+
+        for i in range(1,cant):
+            filename = '../images/scanner/baguette/baguette{}.tif'.format(i)
             print filename
-            nonbread[j] = features(filename,which)
-            j = j+1
-        if (j > 41):
-            break
-
-    for i in range(1,cant):
-        filename = '../images/scanner/baguette/baguette{}.tif'.format(i)
-        print filename
-        baguette[i] = features(filename,which)
-        filename = '../images/scanner/lactal/lactal{}.tif'.format(i)
-        print filename
-        lactal[i] = features(filename,which)
-        filename = '../images/scanner/salvado/salvado{}.tif'.format(i)
-        print filename
-        salvado[i] = features(filename,which)
-        filename = '../images/scanner/sandwich/sandwich{}.tif'.format(i)
-        print filename
-        sandwich[i] = features(filename,which)
+            baguette[i] = callF(filename,which)
+            filename = '../images/scanner/lactal/lactal{}.tif'.format(i)
+            print filename
+            lactal[i] = callF(filename,which)
+            filename = '../images/scanner/salvado/salvado{}.tif'.format(i)
+            print filename
+            salvado[i] = callF(filename,which)
+            filename = '../images/scanner/sandwich/sandwich{}.tif'.format(i)
+            print filename
+            sandwich[i] = callF(filename,which)
 
 
-        v = 50
-        b = 1.05
-        filename = '../images/camera/baguette/slicer/b{}.tif'.format(i)
-        print filename
-        baguetteC[i] = features(filename,which)
-        filename = '../images/camera/lactal/l{}.tif'.format(i)
-        print filename
-        lactalC[i] = features(filename,which)
-        filename = '../images/camera/salvado/s{}.tif'.format(i)
-        print filename
-        salvadoC[i] = features(filename,which)
-        filename = '../images/camera/sandwich/s{}.tif'.format(i)
-        print filename
-        sandwichC[i] = features(filename,which)
+            v = 50
+            b = 1.05
+            filename = '../images/camera/baguette/slicer/b{}.tif'.format(i)
+            print filename
+            baguetteC[i] = callF(filename,which)
+            filename = '../images/camera/lactal/l{}.tif'.format(i)
+            print filename
+            lactalC[i] = callF(filename,which)
+            filename = '../images/camera/salvado/s{}.tif'.format(i)
+            print filename
+            salvadoC[i] = callF(filename,which)
+            filename = '../images/camera/sandwich/s{}.tif'.format(i)
+            print filename
+            sandwichC[i] = callF(filename,which)
    
 
+    trainingData = baguette[1:]+lactal[1:]+salvado[1:]+sandwich[1:]+nonbread[0:20]
+    testingData = baguetteC[1:]+lactalC[1:]+salvadoC[1:]+sandwichC[1:]+nonbread[20:40]
     with open(fileScsv, 'wb') as f:
         writer = csv.writer(f)
-        writer.writerows(baguette[1:]+lactal[1:]+salvado[1:]+sandwich[1:]+nonbread[0:20])
+        writer.writerows(trainingData)
 
     with open(fileCcsv, 'wb') as f:
         writer = csv.writer(f)
-        writer.writerows(baguetteC[1:]+lactalC[1:]+salvadoC[1:]+sandwichC[1:]+nonbread[20:40])
+        writer.writerows(testingData)
+
 
     prog = './a.out' # convert.c
     cmd = '{0} "{1}" > "{2}"'.format(prog, fileScsv, fileStxt)
     Popen(cmd, shell = True, stdout = PIPE).communicate()	
     cmd = '{0} "{1}" > "{2}"'.format(prog, fileCcsv, fileCtxt)
     Popen(cmd, shell = True, stdout = PIPE).communicate()
-    test(fileStxt, fileCtxt, base)
+    labels = [i for i in range(len(testingData))]
+    labels = map(lambda i: i/20+1, labels)
+    test(trainingData, testingData, labels,fileStxt, fileCtxt, base)
 
 
 # SIFT BoW
@@ -204,29 +236,15 @@ import sift
 from glob import glob
 from numpy import zeros, resize, sqrt, histogram, hstack, vstack, savetxt, zeros_like
 import scipy.cluster.vq as vq
-#from cPickle import dump, HIGHEST_PROTOCOL
-#import argparse
-#EXTENSIONS = [".jpg", ".bmp", ".png", ".pgm", ".tif", ".tiff"]
-#DATASETPATH = '../dataset/demosicura'
 PRE_ALLOCATION_BUFFER = 1000  # for sift
-#HISTOGRAMS_FILE = 'trainingdata.svm'
 K_THRESH = 1  # early stopping threshold for kmeans originally at 1e-5, increased for speedup
 CODEBOOK_FILE = 'codebook.file'
 
 # only one file
 def extractSift(filename):
-    #print "extracting Sift features"
-    #all_features_dict = {}
-    #for i, fname in enumerate(input_files):
     features_fname = filename + '.sift'
-    #if exists(features_fname) == False:
-        #print "calculating sift features for", fname
     sift.process_image(filename, features_fname)
-    #print "gathering sift features for", fname,
     locs, descriptors = sift.read_features_from_file(features_fname)
-    #print descriptors.shape
-    #all_features_dict[0] = descriptors
-    #return all_features_dict
     return descriptors
 
 
@@ -337,41 +355,34 @@ def localFeatures(subname):
     all_features_arrayS = dict2numpy(all_featuresS)
     all_features_arrayC = dict2numpy(all_featuresC)
 
-    print "Paso.."
+    print "Kmeans"
     nfeatures = all_features_arrayS.shape[0]
-    print "A"
     nclusters = int(sqrt(nfeatures))
-    print "B"
     # the codebook is made with the training SIFT descriptors
     codebook, distortion = vq.kmeans(all_features_arrayS,
                                              nclusters,
                                              thresh=K_THRESH)
-    print "C"
-    #with open(datasetpath + CODEBOOK_FILE, 'wb') as f:
-
-    #    dump(codebook, f, protocol=HIGHEST_PROTOCOL)
     print "Codebook OK"
     all_word_histgramsS = [[0 for j in range(20)] for i in range(len(all_featuresS))]
     for imagefname in all_featuresS:
         word_histgram = computeHistograms(codebook, all_featuresS[imagefname])
         all_word_histgramsS[imagefname] = word_histgram
 
-    #print "---------------------"
-    print "## computing visual word histograms (2)"
     all_word_histgramsC = [[0 for j in range(20)] for i in range(len(all_featuresC))]
     for imagefname in all_featuresC:
         word_histgram = computeHistograms(codebook, all_featuresC[imagefname])
         all_word_histgramsC[imagefname] = word_histgram
         #print "W", word_histgram
 
-    print "Good Bye!"
     #print all_word_histgramsS, all_word_histgramsC
     return all_word_histgramsS, all_word_histgramsC
 
-#main('gch',0)
-#main('ccv',1)
-#main('haralick',2)
-#main('lbp',3)
-#main('tas',4)
-#main('zernike',5)
-main('sift',6)
+
+#main('gch',0,0)
+#main('ccv',1,0)
+#main('haralick',2,0)
+#main('lbp',3,0)
+#main('tas',4,0)
+#main('zernike',5,0)
+main('sift',6,1)
+
